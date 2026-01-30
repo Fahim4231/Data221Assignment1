052c04e609d25bc85c26c8a4a9858bb3ef2aaaf9
Fahim4231 <ad.fahim2006@gmail.com>
2026-01-30 13:19:41 -0700

    Added Assignment 1 code

---
+++ Data221_Lecture2_PythonReview.py
@@ -0,0 +1,23 @@
+intnum=1
+floatnum=2
+print(intnum)
+print(floatnum)
+string="begin"
+lengthofstring=(len(string))
+print(lengthofstring)
+print(lengthofstring[0])
+print(lengthofstring[0:2])
+lengthofstring.append(5)
+print(lengthofstring)
+lengthofstring.extend([6,['yes',7]])
+print(lengthofstring)
+print(len(string))
+x,y=lengthofstring[0:2]
+print(x)
+print(x)
+print('end' in lengthofstring)
+print('t' in 'start')
+dictionarynum={'one':1, 'two':2, 'three':'begin'}
+print(dictionarynum['two'])
+datadictionary={'projects':50, 'quizes':15, 'midterm':15,'final':20 }
+datadictionary['projects']
