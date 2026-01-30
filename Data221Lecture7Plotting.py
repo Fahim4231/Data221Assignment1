5c5b81c0dc4128f8fd9274804422912d44776e3e
Fahim4231 <ad.fahim2006@gmail.com>
2026-01-30 14:25:56 -0700

    Delete Data221Lecture7Plotting.py

--- Data221Lecture7Plotting.py
+++
@@ -1,5 +0,0 @@
-from sklearn.preprocessing import StandardScaler
-data = [[10, 90], [12, 96], [9, 120], [13, 112], [8, 88]]
-scaler = StandardScaler()
-data = scaler.fit_transform(data)
-print(scaler)
